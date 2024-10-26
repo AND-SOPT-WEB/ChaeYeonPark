86bbac30aeab0d4c37932cf87dd845c710babfe8
Parkchaeyeon <ccyy1029@naver.com>
2024-10-26 16:44:43 +0900

    feat: 화면에 멤버리스트 그리는 기능 추가

--- week2/assignment/index.js
+++ week2/assignment/index.js
@@ -6,36 +6,35 @@ window.localStorage.setItem("membersData", JSON.stringify(members));
 
 const membersData = JSON.parse(localStorage.getItem("membersData"));
 
-const memberList = document.querySelector(".table-content");
+const memberList = document.querySelector(".table-body");
 
-const renderTableHeader = () => {
-  const tableHeader = `
-        <tr>
-      <th>
-        <input type="checkbox" />
-      </th>
-      <th>이름</th>
-      <th>영문이름</th>
-      <th>GitHub</th>
-      <th>성별</th>
-      <th>역할</th>
-      <th>1주차 금잔디</th>
-      <th>2주차 금잔디</th>
-    </tr>`;
+// const renderTableHeader = () => {
+//   const tableHeader = `
+//     <tr>
+//         <th><input type="checkbox" class="table-header-checkbox"/></th>
+//         <th>이름</th>
+//         <th>영문이름</th>
+//         <th>GitHub</th>
+//         <th>성별</th>
+//         <th>역할</th>
+//         <th>1주차 금잔디</th>
+//         <th>2주차 금잔디</th>
+//     </tr>`;
 
-  memberList.innerHTML = tableHeader;
-};
+//   memberList.innerHTML = tableHeader;
+// };
 
 const renderMemberList = (data) => {
-  renderTableHeader();
+  //   renderTableHeader();
+  memberList.textContent = "";
   const memberListData = data.map((member) => {
     return `            
           <tr class="tr">
-                    <td><input type="checkbox" /></td>
+                    <td><input type="checkbox" class="filter-checkbox" /></td>
                     <td>${member.name}</td>
                     <td>${member.englishName}</td>
                     <td>${member.github}</td>
-                    <td>${member.gender}</td>
+                    <td>${member.gender === "male" ? "남자" : "여자"}</td>
                     <td>${member.role}</td>
                     <td>${member.firstWeekGroup}</td>
                     <td>${member.secondWeekGroup}</td>
@@ -58,57 +57,87 @@ const filterInputRole = document.querySelector(".filter-input-role");
 const filterInputWeek1 = document.querySelector(".filter-input-week1");
 const filterInputWeek2 = document.querySelector(".filter-input-week2");
 
+// filterSearchBtn.addEventListener("click", () => {
+//   const filteredMemberList = membersData.reduce((prev, member) => {
+//     const filteredName = filterInputName.value
+//       ? member.name.includes(filterInputName.value)
+//       : true;
+
+//     const filteredEngName = filterInputEngName.value
+//       ? member.englishName
+//           .toLowerCase()
+//           .includes(filterInputEngName.value.toLowerCase())
+//       : true;
+
+//     const filteredGithub = filterInputGithub.value
+//       ? member.github.includes(filterInputGithub.value)
+//       : true;
+
+//     const filteredGender = filterInputGender.value
+//       ? member.gender === filterInputGender.value
+//       : true;
+
+//     const filteredRole = filterInputRole.value
+//       ? member.role === filterInputRole.value
+//       : true;
+
+//     const filteredWeek1 = filterInputWeek1.value
+//       ? member.firstWeekGroup === Number(filterInputWeek1.value)
+//       : true;
+
+//     const filteredWeek2 = filterInputWeek2.value
+//       ? member.secondWeekGroup === Number(filterInputWeek2.value)
+//       : true;
+
+//     if (
+//       filteredName &&
+//       filteredEngName &&
+//       filteredGithub &&
+//       filteredGender &&
+//       filteredRole &&
+//       filteredWeek1 &&
+//       filteredWeek2
+//     ) {
+//       prev.push(member);
+//     }
+
+//     return prev;
+//   }, []);
+
+//   console.log(filteredMemberList);
+
+//   tableBody.textContent = "";
+
+//   renderMemberList(filteredMemberList);
+// });
+
 filterSearchBtn.addEventListener("click", () => {
-  const filteredMemberList = membersData.reduce((prev, member) => {
-    const filteredName = filterInputName.value
-      ? member.name.includes(filterInputName.value)
-      : true;
+  const filteredMemberList = membersData.filter((member) => {
+    const {
+      name,
+      englishName,
+      github,
+      gender,
+      role,
+      firstWeekGroup,
+      secondWeekGroup,
+    } = member;
 
-    const filteredEngName = filterInputEngName.value
-      ? member.englishName
-          .toLowerCase()
-          .includes(filterInputEngName.value.toLowerCase())
-      : true;
-
-    const filteredGithub = filterInputGithub.value
-      ? member.github.includes(filterInputGithub.value)
-      : true;
-
-    const filteredGender = filterInputGender.value
-      ? member.gender === filterInputGender.value
-      : true;
-
-    const filteredRole = filterInputRole.value
-      ? member.role === filterInputRole.value
-      : true;
-
-    const filteredWeek1 = filterInputWeek1.value
-      ? member.firstWeekGroup === Number(filterInputWeek1.value)
-      : true;
-
-    const filteredWeek2 = filterInputWeek2.value
-      ? member.secondWeekGroup === Number(filterInputWeek2.value)
-      : true;
-
-    if (
-      filteredName &&
-      filteredEngName &&
-      filteredGithub &&
-      filteredGender &&
-      filteredRole &&
-      filteredWeek1 &&
-      filteredWeek2
-    ) {
-      prev.push(member);
-    }
-
-    return prev;
-  }, []);
+    return (
+      name.includes(filterInputName.value) &&
+      englishName
+        .toLowerCase()
+        .includes(filterInputEngName.value.toLowerCase()) &&
+      github.toLowerCase().includes(filterInputGithub.value.toLowerCase()) &&
+      gender.includes(filterInputGender.value) &&
+      role.includes(filterInputRole.value) 
+    //   &&
+    //   firstWeekGroup === Number(filterInputWeek1.value) &&
+    //   secondWeekGroup === Number(filterInputWeek2.value)
+    );
+  });
 
   console.log(filteredMemberList);
-
-  memberList.textContent = "";
-
   renderMemberList(filteredMemberList);
 });
 
@@ -125,4 +154,14 @@ filterResetBtn.addEventListener("click", () => {
   filterInputWeek2.value = "";
 
   renderMemberList(membersData);
+});
+
+// 체크박스 전체선택
+const headerCheckbox = document.querySelector(".table-header-checkbox");
+const filterCheckboxAll = document.querySelectorAll(".filter-checkbox");
+
+headerCheckbox.addEventListener("click", () => {
+  filterCheckboxAll.forEach((checkbox) => {
+    checkbox.checked = headerCheckbox.checked;
+  });
 });
